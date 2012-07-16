929e2c919739ad16a2811af2273291614292030d
Toshiaki Baba <toshiaki@netmark.jp>
2012-07-16 17:19:10 +0900

    implemention done.
    - objectid
    - time

--- yaoya_webif/application.py
+++ yaoya_webif/application.py
@@ -6,7 +6,9 @@ from flask import redirect
 from flask import request
 from flask import jsonify
 from pymongo import Connection
+from pymongo import DESCENDING
 from ConfigParser import ConfigParser
+from bson.objectid import ObjectId
 import re
 
 app = Flask(__name__)
@@ -46,20 +48,33 @@ def api_results(group_name,field_name):
     connection = Connection(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     filter_condition={'visible':'True'}
-    if group_name != '*':
-        filter_condition.update({'group_name':group_name})
+    if group_name == '*':
+        return jsonify({'values':''},500)
+    filter_condition.update({'group_name':group_name})
     if field_name != '*' and re.match(r'^command_',field_name):
         filter_condition.update({'command_name':field_name})
-    import sys
-    items = collection.find(filter_condition).sort(field_name)
+    host_names = collection.find(filter_condition).distinct('host_name')
+    host_names.sort()
+    items=[]
+    for host_name in host_names:
+        host_filter = {'visible':'True'}
+        host_filter.update({'group_name':group_name})
+        host_filter.update({'command_name':field_name})
+        host_filter.update({'host_name':host_name})
+        latest=collection.find(host_filter).sort('time',DESCENDING).limit(1).next()
+        items.append(latest)
     jsondata=list()
     for item in items:
         if item == '':
             continue
         else:
-            ## TODO: convert
-            item['_id']=''
-            item['time']=''
+            ## _id
+            item['_id']=str(item['_id'])
+            ## time
+            from datetime import datetime
+            from calendar import timegm
+            timetuple=item['time'].timetuple()
+            item['time']=timegm(timetuple)
         jsondata.append(item)
     connection.disconnect()
     return jsonify({'results':jsondata})
